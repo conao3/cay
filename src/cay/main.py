2b51c4336ac3701b35669e119063d3ff7fea2fbf
Naoya Yamashita <conao3@gmail.com>
2021-08-22 03:42:29 +0900

    implement simple parse

--- src/cay/main.py
+++ src/cay/main.py
@@ -1,7 +1,29 @@
 """Cay: Simple Calculator implementation."""
 
+import enum
 import re
+from dataclasses import dataclass
 from typing import List
+
+
+class TokenType(enum.Enum):
+    """Cay TokenType."""
+
+    integer = enum.auto()
+    float = enum.auto()
+    op = enum.auto()
+    function = enum.auto()
+    rparen = enum.auto()
+    lparen = enum.auto()
+    comma = enum.auto()
+
+
+@dataclass
+class Token:
+    """Cay Token."""
+
+    val: str
+    type: TokenType
 
 
 def tokenize(arg: str) -> List[str]:
@@ -15,7 +37,43 @@ def tokenize(arg: str) -> List[str]:
     ['(', '1', '+', '2', ')', '*', '3']
     """
     return re.sub('([0-9]+)', r" \1 ", arg).split()
+def parse(exps: List[str]) -> List[Token]:
+    """
+    Parse.
+
+    >>> parse(tokenize("1+2"))
+    [Token(val=1, type=<TokenType.integer: 1>),
+     Token(val='+', type=<TokenType.op: 3>),
+     Token(val=2, type=<TokenType.integer: 1>)]
+    """
+    res = []
+    for exp in exps:
+        try:
+            exp = int(exp)
+        except ValueError:
+            try:
+                exp = float(exp)
+            except ValueError:
+                exp = exp
+
+        match exp:
+            case "(":
+                res.append(Token(val="(", type=TokenType.lparen))
+            case ")":
+                res.append(Token(val=")", type=TokenType.rparen))
+            case elm if isinstance(elm, int):
+                res.append(Token(val=elm, type=TokenType.integer))
+            case elm if isinstance(elm, float):
+                res.append(Token(elm, type=TokenType.float))
+            case "+" | "-" | "*" | "/" | "mod" as elm:
+                res.append(Token(elm, type=TokenType.op))
+            case elm:
+                res.append(Token(elm, type=TokenType.function))
+
+    return res
 
 
 if __name__ == "__main__":
-    tokenize(input())
+    while True:
+        print("cay> ", end="")
+        print(parse(tokenize(input())))
--- test/test_cay.py
+++ test/test_cay.py
@@ -14,6 +14,26 @@ class TestTokenize:
         assert main.tokenize("1 + 2 + 42") == ["1", "+", "2", "+", "42"]
 
 
+class TestParse:
+    def test_simple(self):
+        assert main.parse(main.tokenize("1+2")) == [
+            main.Token(val=1, type=main.TokenType.integer),
+            main.Token(val='+', type=main.TokenType.op),
+            main.Token(val=2, type=main.TokenType.integer),
+        ]
+
+    def test_paren(self):
+        assert main.parse(main.tokenize("(1+2)*3")) == [
+            main.Token(val='(', type=main.TokenType.lparen),
+            main.Token(val=1, type=main.TokenType.integer),
+            main.Token(val='+', type=main.TokenType.op),
+            main.Token(val=2, type=main.TokenType.integer),
+            main.Token(val=')', type=main.TokenType.rparen),
+            main.Token(val='*', type=main.TokenType.op),
+            main.Token(val=3, type=main.TokenType.integer),
+        ]
+
+
 def test_main():
     assert 1 == 1
     assert __version__ == "0.1.0"
